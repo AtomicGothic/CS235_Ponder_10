036208b97c45538738a1ed641d758faeb077780b
Allan <allanmarina33@gmail.com>
2020-03-09 23:24:46 -0700

    Edited map.h to match UML
    I declared most functions needed in class for map.h. I tried to implement the insert function.

--- map.h
+++ map.h
@@ -22,22 +22,61 @@ private:
 	BST bst;
 
 public:
+	//DEFAULT CONSTRUCTOR
 	map()
 	{
 		bst = NULL;
 	}
 
+	//COPY CONSTRUCTOR
+	map(const map <K, V>& rhs) { bst = rhs.bst; };
+
+	//DESTRUCTOR
 	~map()
 	{
 		clear();
 	}
 
+	//ASSIGNMENT OPERATOR
+	map& operator = (const map <K, V>& rhs);
+
+	//STANDARD CONTAINER INTERFACES
 	int size() { return bst.size(); }
 	bool empty() { return bst.empty(); }
 	void clear() { bst.clear(); }
 
+	//SPECIFIC CONTAINER INTERFACES
+	void insert(const pair <K, V>& input);
+	void insert(const K& k, const V& v);
+	map& operator [] (const K& k);
+	map &operator [] (const K& k) const;
+
+	//ITERATOR CLASS FOR MAP
+	class iterator;
+	iterator find(K k);
+	iterator begin();
+	iterator end();
+
 
 };
+
+/*************************************************************************
+* INSERT
+**************************************************************************/
+template<class K, class V>
+void map<K, V>::insert(const pair<K, V>& input)
+{
+	BST<T> iterator it = bst.find(pair);
+
+	if (it != NULL)
+	{
+		*it = pair;
+	}
+	else
+	{
+		bst.insert(pair);
+	}
+}
 
 } // namespace custom
 
